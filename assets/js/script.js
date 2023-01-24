62d8f873ab2215b0d19b97d933636ac0c1e1c6bc
alex-501 <68447140+alex-501@users.noreply.github.com>
2023-01-23 22:13:45 -0800

    attempting to add for while keeping site secure by google standards

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function sendMail() {    
+    var body = document.getElementById("cmessage").value;
+    var subject = document.getElementById("cname").value;
+    var email = document.getElementById("cemail").value;
+
+    window.location.replace(`mailto:bjazzjireh@gmail.com?body=${body} - Por Favor regrese un mensaje a ${email}&subject=${subject} busca mas informacion de Jireh`);    
+}
