7d8ab6d6b9bb3ed1f749451ca434a08fd7965c0f
alex-501 <alexisrodriguez830@yahoo.com>
2023-11-03 03:33:17 -0400

    email correction

--- email.js
+++ email.js
@@ -3,7 +3,7 @@ function sendMail() {
     var subject = document.getElementById("csubject").value;
     var email = document.getElementById("cemail").value;
 
-    var mailtoLink = `mailto:alexisrodriguez830@yahoo.com?body=${body} - Por Favor regrese un mensaje a ${email}&subject=${subject} Busca Mas Informacion`;
+    var mailtoLink = `mailto:BjazzJireh@Gmail.com?body=${body} - Por Favor regrese un mensaje a ${email}&subject=${subject} Busca Mas Informacion`;
     
     window.location.href = mailtoLink;
 }
